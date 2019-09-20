2ac2886ea4772e40cdf8017e55c9eee10bbc792a
Ulises <qtulises@gmail.com>
2019-09-20 11:53:15 -0500

    Se agrego la forma general de la ecuacion y se anadio la parte de evaluacion

--- RaicesMetodo1.py
+++ RaicesMetodo1.py
@@ -1,14 +1,20 @@
 #Se importa el modulo de matematicas
 import math
 #se piden los datos necesarios
-fx=input("Inserte la funcion:")
+print("Este programa resuelve ecuaciones cuadraticas de la forma AX^2 + BX + C ")
+A=input("Inserte el valor de A:")
+B=input("Inserte el valor de B:")
+C=input("Inserte el valor de C:")
 xl=int(input("Inserte el primer limite"))
 xu=int(input("Inserte el segundo limite"))
 #se obtiene el primer xr
-
 xr=(xl+xu)/2
 while True:
     #se evalua en la funcion xl xu y xr 
+    fxl=A*(xl*xl)+B*xl+C
+    fxr=A*(xr*xr)+B*xr+C
+    fxu=A*(xu*xu)+B*xu+C
+    #se guarda el valor de xr para calcular el error
     xra=xr
     if (fxl*fxr) > 0:
         xl=xr
