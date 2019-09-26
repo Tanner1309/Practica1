f49a9a107da19e29b36021fbac6782a49994f9bb
Ulises <qtulises@gmail.com>
2019-09-25 22:50:32 -0500

    Se corrigio el tipo de datos para poder hacer las operaciones

--- RaicesMetodo1.py
+++ RaicesMetodo1.py
@@ -5,19 +5,19 @@ import matplotlib.pyplot as plt
 #se piden los datos necesario
 print("Este programa resuelve ecuaciones cuadraticas de la forma AX^2 + BX + C ")
 A=input("Inserte el valor de A:")
-B=input("Inserte el valor de B:")
-C=input("Inserte el valor de C:")
-xl=int(input("Inserte el primer limite"))
-xu=int(input("Inserte el segundo limite"))
+B=input("Inserte el valor de B: ")
+C=input("Inserte el valor de C: ")
+xl=float(input("Inserte el primer limite: "))
+xu=float(input("Inserte el segundo limite: "))
 #se obtiene el primer xr
-xr=(xl+xu)/2
+xr=float((xl+xu)/2)
 c=0
 ve=[]
 while True:
     #se evalua en la funcion xl xu y xr
-    fxl=A*(xl*xl)+B*xl+C
-    fxr=A*(xr*xr)+B*xr+C
-    fxu=A*(xu*xu)+B*xu+C
+    fxl=float(A*(xl*xl)+B*xl+C)
+    fxr=float(A*(xr*xr)+B*xr+C)
+    fxu=float(A*(xu*xu)+B*xu+C)
     #se guarda el valor de xr para calcular el error
     xra=xr
     if (fxl*fxr) > 0:
