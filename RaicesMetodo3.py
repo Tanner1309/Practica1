b243a07a39709913602bc6164c01908544d036fc
Ulises <qtulises@gmail.com>
2019-09-26 11:35:04 -0500

    Se corrigio un error en el metodo2, sin embargo sigue sin obtener el resultado correcto

--- RaicesMetodo3.py
+++ RaicesMetodo3.py
@@ -1,40 +1,33 @@
 #Se importa el modulo de matematicas
 import math
+#Se importa numpy como la palabra np 
 import numpy as np
+#Se importa matplotlib como la palabra plt 
 import matplotlib.pyplot as plt
 #se piden los datos necesario
 print("Este programa resuelve ecuaciones cuadraticas de la forma AX^2 + BX + C ")
 A=int(input("Inserte el valor de A:"))
 B=int(input("Inserte el valor de B: "))
 C=int(input("Inserte el valor de C: "))
-xl=int(input("Inserte el primer limite: "))
-xu=int(input("Inserte el segundo limite: "))
-#se obtienen las primeras evaluaciones
-fxl=A*(xl*xl)+B*xl+C
-fxu=A*(xu*xu)+B*xu+C
+xr=int(input("Inserte el primer punto: "))
 #se obtiene el primer xr
-xr=(fxu*(xl-xu))/(fxl-fxu)
+xr=math.sqrt(((xr+5)/2))
 #se declara la lista donde se guardaran los valores de los errores 
 ve=[]
 c=0
 while True:
-    #se evalua en la funcion xl xu y xr
-    fxl=A*(xl*xl)+B*xl+C
-    fxr=A*(xr*xr)+B*xr+C
-    fxu=A*(xu*xu)+B*xu+C
     #se guarda el valor de xr para calcular el error
     xra=xr
-    if (fxl*fxr) > 0:
-        xl=xr
-    elif (fxu*fxr) > 0:
-        xu=xr
-    xrn=(fxu*(xl-xu))/(fxl-fxu)
+    xrn=math.sqrt(((xr+5)/2))
     #se evalua el error
     e=math.fabs(xrn-xra)
     ve.append(e)
     if e <= 0.01:
         break
-    xr=(fxu*(xl-xu))/(fxl-fxu)
+    elif e>2:
+        print("El metodo no funciona por que diverge")
+        break
+    xrn=xr
     c=c+1
     print (c)
 #se muestra el grafico de error
