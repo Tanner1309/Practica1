059aa9dee398b0c22c76ad9702cbc3d6e3a1112b
Ulises <qtulises@gmail.com>
2019-09-25 22:47:11 -0500

    Se agrego las librarias numpy y matplotlib para poder graficar el error

--- RaicesMetodo1.py
+++ RaicesMetodo1.py
@@ -1,6 +1,8 @@
 #Se importa el modulo de matematicas
 import math
-#se piden los datos necesarios
+import numpy as np
+import matplotlib.pyplot as plt
+#se piden los datos necesario
 print("Este programa resuelve ecuaciones cuadraticas de la forma AX^2 + BX + C ")
 A=input("Inserte el valor de A:")
 B=input("Inserte el valor de B:")
@@ -9,8 +11,10 @@ xl=int(input("Inserte el primer limite"))
 xu=int(input("Inserte el segundo limite"))
 #se obtiene el primer xr
 xr=(xl+xu)/2
+c=0
+ve=[]
 while True:
-    #se evalua en la funcion xl xu y xr 
+    #se evalua en la funcion xl xu y xr
     fxl=A*(xl*xl)+B*xl+C
     fxr=A*(xr*xr)+B*xr+C
     fxu=A*(xu*xu)+B*xu+C
@@ -22,7 +26,14 @@ while True:
         xu=xr
     xrn=(xl+xu)/2
     e=math.fabs(xrn-xra)
+    ve.append(e)
     if e <= 0.1:
         break
     xr=(xl+xu)/2
+    c=c+1    
+#se muestra el grafico de error
+vecE=np.array(ve)
+vecC=np.array(0,c)
+plt.plot(vecC,vecE)
+plt.show()
 
