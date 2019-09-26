39574df59b732de450d23fbed743086cfd0de12c
Ulises <qtulises@gmail.com>
2019-09-25 23:09:29 -0500

    Una vez corregidos los tipos de datos se elimino la variable c y se remplazo por x para tomar el tamano del vector y hacerlo de manera precisa

--- RaicesMetodo1.py
+++ RaicesMetodo1.py
@@ -4,20 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 #se piden los datos necesario
 print("Este programa resuelve ecuaciones cuadraticas de la forma AX^2 + BX + C ")
-A=input("Inserte el valor de A:")
-B=input("Inserte el valor de B: ")
-C=input("Inserte el valor de C: ")
-xl=float(input("Inserte el primer limite: "))
-xu=float(input("Inserte el segundo limite: "))
+A=int(input("Inserte el valor de A:"))
+B=int(input("Inserte el valor de B: "))
+C=int(input("Inserte el valor de C: "))
+xl=int(input("Inserte el primer limite: "))
+xu=int(input("Inserte el segundo limite: "))
 #se obtiene el primer xr
-xr=float((xl+xu)/2)
-c=0
+xr=(xl+xu)/2
+
 ve=[]
 while True:
     #se evalua en la funcion xl xu y xr
-    fxl=float(A*(xl*xl)+B*xl+C)
-    fxr=float(A*(xr*xr)+B*xr+C)
-    fxu=float(A*(xu*xu)+B*xu+C)
+    fxl=A*(xl*xl)+B*xl+C
+    fxr=A*(xr*xr)+B*xr+C
+    fxu=A*(xu*xu)+B*xu+C
     #se guarda el valor de xr para calcular el error
     xra=xr
     if (fxl*fxr) > 0:
@@ -30,10 +30,11 @@ while True:
     if e <= 0.1:
         break
     xr=(xl+xu)/2
-    c=c+1    
+   
 #se muestra el grafico de error
 vecE=np.array(ve)
-vecC=np.array(0,c)
-plt.plot(vecC,vecE)
+x= vecE.size
+vecX=np.arange(0,x)
+plt.plot(vecX,vecE)
 plt.show()
 
