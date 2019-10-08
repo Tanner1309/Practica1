81058df1571bbde0d6a5e697e227e53c3e93126c
Ulises <qtulises@gmail.com>
2019-10-08 12:27:36 -0500

    Se dividieron las graficas para una mejor visualizacion

--- RaicesMetodo3.py
+++ RaicesMetodo3.py
@@ -14,10 +14,12 @@ xr=int(input("Inserte el primer punto: "))
 xr=math.sqrt((-B*xr-C)/A)
 #se declara la lista donde se guardaran los valores de los errores 
 ve=[]
+xrr=[]
 c=0
 while True:
     #se guarda el valor de xr para calcular el error
     xra=xr
+    xrr.append(xr)
     xrn=math.sqrt((-B*xr-C)/A)
     #se evalua el error
     e=math.fabs(xra-xrn)
@@ -25,17 +27,25 @@ while True:
     if e <= 0.000001 or e == 0.0:
         break
     xr=xrn
+    
     c=c+1
 #se muestra el grafico de error
 #se pasa la lista a arreglo
 vecE=np.array(ve)
+vecXr=np.array(xrr)
 #se obtiene el tamano del vector 
 x= vecE.size
 #se crea el vector x
 vecX=np.arange(0,x)
 print("El resultado mas cercano es:",xr)
 #se hace la grafica
+plt.figure()
+plt.subplot(2,1,1)
 plt.plot(vecX,vecE, "go--", linewidth=2, markersize=5)
+plt.title("$Error$")
+plt.subplot(2,1,2)
+plt.plot(vecX,vecXr, "bo-", linewidth=2, markersize=5)
+plt.title("$Resultados$")
 #se muestra la grafica
 plt.show()
 plt.savefig("Metodo3.png")
